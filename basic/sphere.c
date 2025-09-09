6b9cdb40c3454668ec11c3f87ade31f97e76ff5b
Cu pid <kelanidarasimi9@gmail.com>
2025-09-09 05:22:10 +0100

    chore: calculate volume of a sphere

---
+++ basic/sphere.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define PI 3.14159265359f
+
+int main() {
+  float radius, volume;
+  scanf("%f", &radius);
+
+  volume = (4.0f / 3.0f) * PI * (radius * radius * radius);
+  printf("volume of sphere is %.2fm³\n", volume);
+}
