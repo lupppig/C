fe397b7d00d1c2f5a170958aa472b695182bea9b
Cu pid <kelanidarasimi9@gmail.com>
2025-09-09 05:32:27 +0100

    chore: a program that asks the user to enter a dollars-and-cents amount, then displays the amount with 5% tax added

---
+++ basic/tax.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  float amount, tax_added, tax_rate;
+
+  printf("Enter an amount: ");
+  scanf("%f", &amount);
+
+  tax_rate = 0.05f;
+  tax_added = amount * (1 + tax_rate);
+  printf("with tax added: $%.2f\n", tax_added);
+}
